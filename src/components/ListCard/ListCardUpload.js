431eaa475dee318a66a0b64c31277f77bccbfeda
PhongND <phongnd@bkav.com>
2021-03-15 13:18:45 +0700

    use component schedule

--- src/components/ListCard/ListCardUpload.js
+++ src/components/ListCard/ListCardUpload.js
@@ -1,17 +1,19 @@
-import { List, Card } from 'antd';
-import { useEffect } from 'react';
+import { List } from 'antd';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { GetDataDocument } from '../../services/DocumentService';
 import CardUpload from './CardUpload';
 export default function ListCardUpload(){
     const documentRedux = useSelector(state=>state.Document.documents)
+    const [data,setData] = useState(documentRedux);
     const dispatch = useDispatch()
     useEffect(()=>{
       GetDataDocument(dispatch)
       .then(documents =>{
         console.log(documents);
       })
-    },[])
+      console.log('render');
+    },[data])
     return(
         <div className="ListCardUpload">
         <List
--- src/pages/Schemal/TableSchemal.js
+++ src/pages/Schemal/TableSchemal.js
@@ -1,75 +1,51 @@
-import { Badge, Calendar } from "antd";
+import { Badge } from "antd";
 import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
 import './Schemal.scss';
 export default function TableSchemal(){
-   
+  const data = [
+          {
+              Id: 1,
+              Subject: 'Explosion of Betelgeuse Star',
+              StartTime: new Date(2021, 1, 15, 5, 30),
+              EndTime: new Date(2021, 1, 15, 7, 0)
+          }, {
+              Id: 2,
+              Subject: 'Thule Air Crash Report',
+              StartTime: new Date(2021, 1, 16, 0, 0),
+              EndTime: new Date(2021, 1, 16, 2, 0)
+          }, {
+              Id: 3,
+              Subject: 'Blue Moon Eclipse',
+              StartTime: new Date(2021, 1, 17, 2, 0),
+              EndTime: new Date(2021, 1, 17, 4, 0)
+          }, {
+              Id: 4,
+              Subject: 'Meteor Showers in 2021',
+              StartTime: new Date(2021, 1, 14, 0, 0),
+              EndTime: new Date(2021, 1, 14, 2, 30)
+          },
+          {
+            Id: 5,
+              Subject: 'Explosion of Betelgeuse Star',
+              StartTime: new Date(2021, 1, 15, 3, 0),
+              EndTime: new Date(2021, 1, 15, 5, 0)
+          }, {
+              Id: 6,
+              Subject: 'Thule Air Crash Report',
+              StartTime: new Date(2021, 1, 19, 2, 0),
+              EndTime: new Date(2021, 1, 19, 4, 0)
+          }, {
+              Id: 7,
+              Subject: 'Blue Moon Eclipse',
+              StartTime: new Date(2021, 1, 18, 1, 30),
+              EndTime: new Date(2021, 1, 18, 3, 0)
+          },];
     return(
         <div className="tabelPanel">
-            <ScheduleComponent>
+            <ScheduleComponent height='550px' selectedDate={new Date(2021, 1, 15)} eventSettings={{dataSource:data}}>
                 <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
             </ScheduleComponent>
-            {/* <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} /> */}
         </div>
         
     );
 }
-function getListData(value) {
-    let listData;
-    console.log(value.day()+2);
-    switch (value.day()+2) {
-      case 2:
-        listData = [
-          { type: 'warning', content: '1810A05' },
-          { type: 'success', content: '1810A05' },
-        ];
-        break;
-      case 3:
-        listData = [
-          { type: 'warning', content: '1810A05' },
-          { type: 'success', content: '1810A05' },
-          { type: 'error', content: '1810A05' },
-        ];
-        break;
-      case 4:
-        listData = [
-          { type: 'warning', content: '1810A05' },
-          { type: 'success', content: '1810A05' },
-          { type: 'error', content: 'This is error event 1.' },
-          { type: 'error', content: 'This is error event 2.' },
-          { type: 'error', content: 'This is error event 3.' },
-          { type: 'error', content: 'This is error event 4.' },
-        ];
-        break;
-      default:
-    }
-    return listData || [];
-  }
-  
-  function dateCellRender(value) {
-    const listData = getListData(value);
-    return (
-      <ul className="events">
-        {listData.map(item => (
-          <li key={item.content}>
-            <Badge status={item.type} text={item.content} />
-          </li>
-        ))}
-      </ul>
-    );
-  }
-  
-  function getMonthData(value) {
-    if (value.month() === 8) {
-      return 1394;
-    }
-  }
-  
-  function monthCellRender(value) {
-    const num = getMonthData(value);
-    return num ? (
-      <div className="notes-month">
-        <section>{num}</section>
-        <span>Backlog number</span>
-      </div>
-    ) : null;
-  }
