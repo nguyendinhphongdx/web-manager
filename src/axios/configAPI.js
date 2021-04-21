1f1979eefc514f5bea90a123f6165f032f0631a6
nguyendinhphongdx <phongnguyendx@gmail.com>
2021-04-21 20:01:16 +0700

    fix

--- src/axios/configAPI.js
+++ src/axios/configAPI.js
@@ -1,8 +1,8 @@
 // export const NODE_SERVER = "https://management--system.herokuapp.com";
 // export const NODE_SOCKET = "https://management--system.herokuapp.com:5050";
-// export const NODE_SERVER = "http://localhost:5000";
-// export const NODE_SOCKET = "http://localhost:5050";
+export const NODE_SERVER = "http://localhost:5000";
+export const NODE_SOCKET = "http://localhost:5050";
 // export const NODE_SERVER = "http://27.72.28.152:5000";
 // export const NODE_SOCKET = "http://27.72.28.152:5050";
-export const NODE_SERVER = "http://13.59.237.131:5000";
-export const NODE_SOCKET = "http://13.59.237.131:5050";
+//export const NODE_SERVER = "http://13.59.237.131:5000";
+//export const NODE_SOCKET = "http://13.59.237.131:5050";
